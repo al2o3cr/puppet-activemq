2604514f1bd799e366c6c314119627d3c0274c83
Paul Carey <paul.carey@ignition.it>
2014-05-29 10:51:36 +0100

    Update activemq location

--- files/brews/activemq.rb
+++ files/brews/activemq.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Activemq < Formula
   homepage 'http://activemq.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=activemq/apache-activemq/5.8.0/apache-activemq-5.8.0-bin.tar.gz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz'
   sha1 '426150ee260d3ab57e9c8674944ca7d171646056'
   version '5.8.0-boxen1'
 
