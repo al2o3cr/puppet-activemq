9180b1843cd4091de0eef9d1f0ec8484e8c75d48
Matt Jones <al2o3cr@gmail.com>
2014-10-30 15:56:42 -0400

    Fix SHA for ActiveMQ 5.9.1 download
    No idea how this ever worked for srclab...

--- files/brews/activemq.rb
+++ files/brews/activemq.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Activemq < Formula
   homepage 'http://activemq.apache.org/'
   url 'http://www.apache.org/dyn/closer.cgi?path=/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz'
-  sha1 '426150ee260d3ab57e9c8674944ca7d171646056'
+  sha1 '03c0730e8de406ad8dc243f5834bf5b74492c5e8'
   version '5.9.1-boxen1'
 
   skip_clean 'libexec/webapps/admin/WEB-INF/jsp'
